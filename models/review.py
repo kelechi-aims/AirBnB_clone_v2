c18c390c9ce017f654dfe101d58506ae580efb41
Tolulope Abiola <anotibills@gmail.com>
2023-09-20 21:43:33 +0100

    Tasks 7 to 10

--- models/review.py
+++ models/review.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+""" State Module for HBNB project """
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
 
-
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """
+    Represents a review for a MySQL database.
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
