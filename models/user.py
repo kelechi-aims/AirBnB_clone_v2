8793302327c21a698c33777c398d2a9d0406a9cd
Tolulope Abiola <anotibills@gmail.com>
2023-09-21 02:33:37 +0100

    user_1

--- models/user.py
+++ models/user.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
+"""This is the user class"""
+
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+from models.base_model import BaseModel, Base
+
+
 class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        email: the email address
+        password: the password for your login
+        first_name: user first name
+        last_name: user last name
     """
-    Represents a user for a MySQL database.
-    """
-    __tablename__ = "users"
+    __tablename__ = 'users'
+    
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+    reviews = relationship("Review", backref="user")
