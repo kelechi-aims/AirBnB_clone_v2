e4611e40e08c06a15a61457edab0813dc14df06d
kelechi-aims <kelechi@aims.edu.gh>
2023-09-22 09:02:55 +0100

    task

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,8 +2,7 @@
 """
 Module db_storage
 """
-
-import os
+from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import BaseModel, Base
@@ -14,13 +13,11 @@ from models.place import Place
 from models.user import User
 from models.review import Review
 
-database = os.getenv("HBNB_MYSQL_DB")
-host = os.getenv("HBNB_MYSQL_HOST")
-password = os.getenv("HBNB_MYSQL_PWD")
-user = os.getenv("HBNB_MYSQL_USER")
-
-instances = {"State": State, "City": City, "Amenity": Amenity,
-             "User": User, "Review": Review, "Place": Place}
+db = getenv("HBNB_MYSQL_DB")
+host = getenv("HBNB_MYSQL_HOST")
+passwd = getenv("HBNB_MYSQL_PWD")
+user = getenv("HBNB_MYSQL_USER")
+env = getenv("HBNB_ENV")
 
 
 class DBStorage():
@@ -35,46 +32,48 @@ class DBStorage():
         Creates the engine
         """
         self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}"
-                                      .format(user, password, host, database),
+                                      .format(user, passwd, host, db),
                                       pool_pre_ping=True)
-        if os.getenv("HBNB_ENV") == "test":
-            Base.metadata.drop_all(bind=self.__engine)
+        if env == "test":
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """
         Prints all objects in the database
         """
-        instlist = [State, City, User]
-        new_dict = {}
+        obj_dict = {}
         if cls:
-            for obj in self.__session.query(instances[cls]).all():
-                key = str(obj.__class__.__name__) + "." + str(obj.id)
-                value = obj
-                new_dict[key] = value
-            return new_dict
+            objects = self.__session.query(cls).all()
         else:
-            for inst in instlist:
-                for obj in self.__session.query(inst).all():
-                    key = str(inst.__name__) + "." + str(obj.id)
-                    value = obj
-                    new_dict[key] = value
-            return new_dict
+            objects = []
+            classes = [User, State, City, Amenity, Place, Review]
+            for cls in classes:
+                objects += self.__session.query(cls).all()
+        for obj in objects:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            obj_dict[key] = obj
+        return obj_dict
 
-     def reload(self):
-        Base.metadata.create_all(bind=self.__engine)
+    def new(self, obj):
+        """ Add the object to the current database session """
+        self.__session.add(obj)
+
+    def save(self):
+        """ Commit all changes of the current database session """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ Delete from the current database session obj if not None """
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """ Create all tables in the database (feature of SQLAlchemy)"""
+        Base.metadata.create_all(self.__engine)
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Scoped = scoped_session(Session)
         self.__session = Scoped
 
-    def new(self, obj):
-        self.__session.add(obj)
-
-    def save(self):
-        self.__session.commit()
-
-    def delete(self, obj=None):
-        if obj is not None:
-            self.__session.delete(obj)
-
     def close(self):
+        """ closes the session """
         self.__session.remove()
