49bda6620aa0993ab06114b0190ec86ad6ca17ec
kelechi-aims <kelechi.denise@yahoo.com>
2023-10-08 18:05:59 +0000

    task-2 update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,46 @@
 #!/usr/bin/python3
 """
-Fabric script to distribute an archive to your web servers
+Fabric script (based on the file 1-pack_web_static.py) that distributes
+an archive to your web servers, using the function do_deploy
 """
 from fabric.api import run, put, env
-from os.path import exists
+import os
 
 
 env.hosts = ['54.234.57.34', '54.89.143.165']
-env.user = 'ubuntu'
+
 
 
 def do_deploy(archive_path):
     """
     Distributes an archive to your web servers
     """
-    if not exists(archive_path):
+    if not os.path.exists(archive_path):
         return False
 
     try:
-        filename = archive_path.split('/')[-1]
-        folder_name = '/data/web_static/releases/{}'.format(
-            filename.strip('.tgz')
-        )
-
         # Upload the archive to /tmp/ directory on the server
         put(archive_path, '/tmp/')
-        run('mkdir -p {}'.format(folder_name))
-        run('tar -xzf /tmp/{} -C {}'.format(filename, folder_name))
+        # Extract archive to the folder /data/web_static/releases/
+        archive_filename = os.path.basename(archive_path)
+        archive_folder = '/data/web_static/releases/' + archive_filename.split('.')[0]
+        run('mkdir -p {}'.format(archive_folder))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, archive_folder))
 
-        # Remove the archive from the server
-        run('rm /tmp/{}'.format(filename))
+        # Delete the uploaded archive from the web server
+        run('rm /tmp/{}'.format(archive_filename))
 
-        # Move files to correct location
-        run('mv {}/web_static/* {}'.format(folder_name, folder_name))
+        # Move the extracted content to the correct location
+        run('mv {}/web_static/* {}'.format(archive_folder, archive_folder))
 
-        # Remove unnecessary directory
-        run('rm -rf {}/web_static'.format(folder_name))
-
-        # Remove the old symbolic link
+        # Remove the symbolic link /data/web_static/current
         run('rm -rf /data/web_static/current')
 
         # Create a new symbolic link
-        run('ln -s {} /data/web_static/current'.format(folder_name))
+        run('ln -s {} /data/web_static/current'.format(archive_folder))
 
         print("New version deployed!")
         return True
+
     except Exception:
         return False
