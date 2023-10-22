3023a38488648892b4c523ac5f644ec04346c343
kelechi-aims <kelechi@aims.edu.gh>
2023-10-22 06:43:45 +0100

    task-6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+'''Script that starts a Flask web application'''
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''Route handler function for the root URL ("/")'''
+    return "Hello HBNB!"
+
+
+@app.route('/hnbn', strict_slashes=False)
+def hbnb():
+    ''' Route  handler function for the "/hbnb" URL.'''
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    ''' Route handler function for the "/c/<text>" URL pattern.'''
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    ''' Route handler function for the "/python/<text>" URL pattern.'''
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def show_number(n):
+    """ Route handler function for the '/number/<n>' URL pattern."""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ Route handler function for the "/number_template/<n>" URL pattern. """
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """
+    Route handler function for the "/number_odd_or_even/<n>" URL pattern.
+    """
+    return render_template('6-number_odd_or_even.html', number=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
