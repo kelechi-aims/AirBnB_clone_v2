adf0ced28b93f3668a201b110aa04db20aa13a89
kelechi-aims <kelechi@aims.edu.gh>
2023-10-22 05:48:54 +0100

    task-3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''Script that starts a Flask web application'''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''Route handler function for the root URL ("/")'''
+    return "Hello HBNB!"
+
+
+@app.route('/hnbn', strict_slashes=False)
+def hbnb():
+    ''' Route  handler function for the "/hbnb" URL.'''
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    ''' Route handler function for the "/c/<text>" URL pattern.'''
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    ''' Route handler function for the "/python/<text>" URL pattern.'''
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
