631c76ee861ab206cdd580f9cbde45913c744830
kelechi-aims <kelechi@aims.edu.gh>
2023-10-25 10:06:42 +0100

    task-9 correction

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,15 +9,15 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
+    """ Rremove the current SQLAlchemy Session """
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    states = storage.all("State").values()
-    return render_template('cities_by_states.html', states=states)
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
